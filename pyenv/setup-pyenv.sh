fb2acad75ef5fb58874f09412c68f89e1dea4007
Daath <huangxuankai93@gmail.com>
2022-12-17 01:42:39 +0800

    update

--- pyenv/setup-pyenv.sh
+++ pyenv/setup-pyenv.sh
@@ -13,8 +13,10 @@ curl https://pyenv.run | bash
 
 # setup pyenv environment
 cat << EOF >> ${LOCAL_ENV}
-export PYENV_ROOT=${HOME}/.pyenv
-export PATH=${PYENV_ROOT}/bin:$PATH
+export PYENV_ROOT="$HOME/.pyenv"
+echo $PATH
+echo $PYENV_ROOT/bin
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 EOF
