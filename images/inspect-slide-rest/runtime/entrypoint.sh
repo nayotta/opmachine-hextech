3d5eab0ce48aef15f65c2ca1cfc092dc049b58d8
huangxuankai <huangxuankai93@gmail.com>
2023-02-02 11:42:00 +0800

    add isr-go

--- images/inspect-slide-rest/runtime/entrypoint.sh
+++ images/inspect-slide-rest/runtime/entrypoint.sh
@@ -10,6 +10,11 @@ echo "[$(date +"%Y-%m-%dT%H:%M:%S-%Z")] ---------- run meta ---------"
 
 sleep '10s'
 
+echo "[$(date +"%Y-%m-%dT%H:%M:%S-%Z")] ---------- run isr-go -------"
+/workspace/run-isr-go.sh &
+
+sleep '5s'
+
 echo "[$(date +"%Y-%m-%dT%H:%M:%S-%Z")] ---------- run inspecet-slide-rest -------"
 /workspace/run-inspect.sh &
 
@@ -22,17 +27,21 @@ while sleep 20; do
         PROCESS_2_STATUS=$?
         ps aux |grep python |grep run |grep -q -v grep
         PROCESS_3_STATUS=$?
+        ps aux |grep isr-go.amd64.linux |grep -q -v grep
+        PROCESS_4_STATUS=$?
+
         # If the greps above find anything, they exit with 0 status
         # If they are not both 0, then something is wrong
-        echo "current process status: P1 $PROCESS_1_STATUS | P2 $PROCESS_2_STATUS | P3 $PROCESS_3_STATUS"
-        if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 ]; then
-                echo "One of the processes has already exited. P1 $PROCESS_1_STATUS | P2 $PROCESS_2_STATUS | P3 $PROCESS_3_STATUS"
+        echo "current process status: P1 $PROCESS_1_STATUS | P2 $PROCESS_2_STATUS | P3 $PROCESS_3_STATUS | P4 $PROCESS_4_STATUS"
+        if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 -o $PROCESS_4_STATUS -ne 0 ]; then
+                echo "One of the processes has already exited. P1 $PROCESS_1_STATUS | P2 $PROCESS_2_STATUS | P3 $PROCESS_3_STATUS | P4 $PROCESS_4_STATUS"
                 pgrep -la -f metathings
                 pid1=$(pgrep -f "metathings.*device")
                 pid2=$(pgrep -f "metathings.*module")
                 pid3=$(pgrep -f "python.*run")
-                echo "kill process device(${pid1}), module(${pid2}), inspect(${pid3})"
-                kill -9 ${pid1} ${pid2} ${pid3}
+                pid4=$(pgrep -f "isr-go.amd64.linux")
+                echo "kill process device(${pid1}), module(${pid2}), inspect(${pid3}) isr-go(${pid4})"
+                kill -9 ${pid1} ${pid2} ${pid3} ${pid4}
                 exit 1
         fi
 done
---
+++ images/inspect-slide-rest/runtime/run-isr-go.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+cd /workspace
+APP_CONFIG=/workspace/etc/isr-go-config.yaml bin/isr-go.amd64.linux
