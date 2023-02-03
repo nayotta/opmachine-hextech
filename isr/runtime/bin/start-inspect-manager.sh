eca96ac51bffda02a821923183f229677504d7e2
huangxuankai <huangxuankai93@gmail.com>
2023-02-03 17:24:04 +0800

    fix start inspect manager

--- isr/runtime/bin/start-inspect-manager.sh
+++ isr/runtime/bin/start-inspect-manager.sh
@@ -3,6 +3,7 @@
 export APP_CONFIG=/home/cloud/workspaces/inspect-slide-rest/runtime/etc/inspect/config.json
 
 cd /home/cloud/workspaces/inspect-slide-rest/runtime/lib/inspect-slide-rest
+(eval "$(pyenv init -)" && pyenv shell 3.10.7 && python -m venv venv)
 . venv/bin/activate
 
 python src/run.py
